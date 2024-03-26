ecb22b4c5731e4d15230f289d9684a4f7f7653a6
Attilio Andrea Marotta <attilio@mekmo.studio>
2024-03-26 11:07:56 +0000

    Set up Database and created models

---
+++ models/Tracks.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const trackSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  version: String,
+  artist: String,
+  isrc: {
+    type: String,
+    required: true,
+  },
+  pLine: String,
+  aliases: [String],
+  contractId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Contract",
+  },
+});
+export default mongoose.model("Track", trackSchema);
